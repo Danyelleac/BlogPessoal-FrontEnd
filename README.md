c35adaa760b5d7697b0abc9cb9f54524b4cb7cdf
Dany <danyelleacandido@gmail.com>
2022-05-08 16:01:01 -0700

    subindo atualizações

---
+++ README.md
@@ -0,0 +1,30 @@
+👉🏽🛠⚠ em construção
+<!--👉🏽🛠⚠ em construção
+✋🏽❌⛔parado
+👍🏽✔🟢concluído-->
+# PROJETO BLOG PESSOAL- FRONTEND
+
+Neste projeto farei o Front End de um Blog utilizando HTML,CSS,JS e ReactJS.
+
+## 🚀 Começando
+
+Acesse a branch código comentado lá encontrará explicações e detalhes sobre a realização do código
+
+## 📸 Imagens/Videos dos Projetos
+
+<img src="">
+
+## 🖇️ Colaborando
+
+Sinta-se a vontade em colaborar com meu projeto.
+
+## 📌 Versão
+
+Versão 1.0;
+
+## 🎁 Expressões de gratidão
+
+* Obrigada @Generation e @Yuri 📢🤓.
+
+---
+🤝🏽inspirado no readme do @lohhans
---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Grid, Paper} from '@material-ui/core';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
+
+function App() {
+  return (
+    <Router>
+    <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> 
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/usuarios/cadastrar" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/postagens" element={<ListaPostagem />} />
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+          <Route path="/formularioTema" element={<CadastroTema />} />
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+          <Route path="/formularioPostagem/:id" element={<DeletarPostagem />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+        </Routes>
+      </div>
+    <Footer />
+</Router>
+);
+}
+
+export default App;
